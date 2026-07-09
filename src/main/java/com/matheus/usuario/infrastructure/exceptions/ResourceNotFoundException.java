c666e11ece119d3d7a4f79b2c3a1e79cbabf587f
Matheus <matheusepilef@hotmail.com>
2026-07-08 22:17:09 -0300

    Adicionados os metodos Get,Delete e Login

---
+++ src/main/java/com/matheus/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.matheus.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException (String mensagem){
+
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+
+        super(mensagem, throwable);
+    }
+
+
+
+}
--- src/main/java/com/matheus/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/matheus/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.matheus.usuario.infrastructure.repository;
 
-import com.matheus.aprendendo_spring.insfrastructure.entity.Endereco;
+
+import com.matheus.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

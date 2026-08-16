026c168aaca3d3fc273fb355964022563d005146
Matheus <matheusepilef@hotmail.com>
2026-08-16 19:16:13 -0300

    Adicionada consulta na API externa do ViaCep

---
+++ src/main/java/com/matheus/usuario/infrastructure/exceptions/IllegalArgumentException.java
@@ -0,0 +1,12 @@
+package com.matheus.usuario.infrastructure.exceptions;
+
+public class IllegalArgumentException extends RuntimeException{
+
+    public IllegalArgumentException (String mensagem){
+        super(mensagem);
+    }
+
+    public IllegalArgumentException (String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
